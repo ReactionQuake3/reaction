8153444877c9b4c58a04f5db5400fad1b4fc70ea
Richard Allen <ra@ra.is>
2011-04-09 21:13:54 +0000

    Fixing UI .so/dll loading

--- reaction/code/ui/ui_syscalls.c
+++ reaction/code/ui/ui_syscalls.c
@@ -31,7 +31,7 @@
 
 static int (QDECL * syscall) (int arg, ...) = (int (QDECL *) (int, ...)) -1;
 
-void dllEntry(int (QDECL * syscallptr) (int arg, ...))
+Q_EXPORT void dllEntry(int (QDECL * syscallptr) (int arg, ...))
 {
 	syscall = syscallptr;
 }
