fd2ed93c9049a6f725d27cbb0271a290a8c091ec
Richard Allen <ra@ra.is>
2011-07-19 15:01:12 +0000

    IOQ3 commit 2095

--- reaction/cross-make-mingw64.sh
+++ reaction/cross-make-mingw64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CMD_PREFIX="i686-pc-mingw32 i586-mingw32msvc i686-w64-mingw32";
+CMD_PREFIX="amd64-mingw32msvc x86_64-w64-mingw32";
 
 if [ "X$CC" = "X" ]; then
     for check in $CMD_PREFIX; do
@@ -26,6 +26,6 @@ if [ "X$WINDRES" = "X" -o "X$CC" = "X" ]; then
 fi
 
 export PLATFORM=mingw32
-export ARCH=x86
+export ARCH=x64
 
 exec make $*
