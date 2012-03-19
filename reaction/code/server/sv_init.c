86acd69e03f4b6240e3b9f54fe2b041998c1a5cb
Richard Allen <ra@ra.is>
2012-03-19 20:13:18 +0000

    Some saner defaults to cvars

--- reaction/code/server/sv_init.c
+++ reaction/code/server/sv_init.c
@@ -670,7 +670,7 @@ void SV_Init (void)
 	Cvar_Get ("nextmap", "", CVAR_TEMP );
 
 	sv_allowDownload = Cvar_Get ("sv_allowDownload", "0", CVAR_SERVERINFO);
-	Cvar_Get ("sv_dlURL", "", CVAR_SERVERINFO | CVAR_ARCHIVE);
+	Cvar_Get ("sv_dlURL", "http://www.rq3.com/download", CVAR_SERVERINFO | CVAR_ARCHIVE);
 	
 	sv_master[0] = Cvar_Get("sv_master1", MASTER_SERVER_NAME, 0);
 	sv_master[1] = Cvar_Get("sv_master2", "master.ioquake3.org", 0);
