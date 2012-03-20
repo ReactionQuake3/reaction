89cb7951fa7e7b585926671f97f6f9467a153c31
Richard Allen <ra@ra.is>
2012-03-20 17:58:57 +0000

    minifix

--- reaction/doit.sh
+++ reaction/doit.sh
@@ -3,8 +3,9 @@
 # Script to set up after build on JB's system
 #
 cd build/release-linux-x86_64/
-cp Reaction* /usr/local/games/Reaction/
+cp Reaction* *.so /usr/local/games/Reaction/
 cd Boomstick
+cp *.so /usr/local/games/Reaction/Boomstick/
 rm -f /usr/local/games/Reaction/Boomstick/zzzzzzzzzzzzzzzzzzzz-code.pk3
 zip -r /usr/local/games/Reaction/Boomstick/zzzzzzzzzzzzzzzzzzzz-code.pk3 vm/
 
